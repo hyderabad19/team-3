625b2b1eca16c0493a5c8ae7ffa6dbfb2f7305fe
Vaishnavi <msreevaishnavi@gmail.com>
2019-07-13 20:12:37 +0530

    login updates

--- Desktop/login.php
+++ Desktop/login.php
@@ -1,76 +1,15 @@
 <?php
-	$connect = new mysqli('localhost','root','','signup');
-	$name=$_POST['name'];
-	$pass=$_POST['pass'];
-	$sql = "SELECT * FROM signup WHERE Name = '$name' and Password = '$pass'";
-	$query=mysqli_query($connect,$sql);
-      
-      	$count = mysqli_num_rows($query);
-	if($count == 1) 
-	{
-		echo "<script>
-		alert('Login Sucessful!!');
-		</script>";
-		
-		echo "Welcome  ";
-		echo $name;	
-		echo "!!!";
-		echo "<br><br><br><br>";
-		echo "YOUR PREVIOUS ACHIEVEMENTS:";
-		$connect = new mysqli('localhost','root','','achievementdatabase');
-	
-		if($connect)
-		{	
-			$sql="select * from achievementtable where Name= '$name'";
-			$query=mysqli_query($connect,$sql);
-			$rowcount=mysqli_num_rows($query);
-		}
-?>
-<table border="1">
-<tr>
-	<td bgcolor=#FA8072>Name</td>
-	<td bgcolor=#FA8072>Achievement title</td>
-	<td bgcolor=#FA8072>Date</td>
-	<td bgcolor=#FA8072>Description</td>
-	<td bgcolor=#FA8072>Subject</td>
-	<td bgcolor=#FA8072>Topic</td>
-	<td bgcolor=#FA8072>Location</td>
-	<td bgcolor=#FA8072>Type of Achievement</td>
-	<td bgcolor=#FA8072>Proof</td>
-</tr>
-<?php
-	for($i=1;$i<=$rowcount;$i++)
-	{
-		$row=mysqli_fetch_array($query);
-	?>
-	<tr>
-	<td><?php echo $row['Name'] ?></td>
-	<td><?php echo $row['Achievement title'] ?></td>
-	<td><?php echo $row['Date'] ?></td>
-	<td><?php echo $row['Description'] ?></td>
-	<td><?php echo $row['Subject'] ?></td>
-	<td><?php echo $row['Topic'] ?></td>
-	<td><?php echo $row['Location'] ?></td>
-	<td><?php echo $row['Type of Achievement'] ?></td>
-	<td><a href="pdfs/<?php echo $row['Proof'] ?>" target="_blank"><?php echo $row['Proof'] ?></a></td>
-	</tr>
-	<?php
-	}
-	?>
-	</table>
-<?php
-
+session_start();
+$email=$_POST["username"];
+$pwd=$_POST["password"];
+$_SESSION["email"]=$email;
+$conn = new mysqli("localhost","root","","cfg");
+$sql  = "select * from teacher where email ='$email' and password ='$pwd'";
+$res = mysqli_query($conn,$sql);
+$cnt = mysqli_num_rows($res);
+if($cnt==1){
+  header("location:userdashboard.html");
+}else{
+  echo"NOT UNABE";
 }
-	else 
-	{
-         		echo "<script>
-		alert('Your username or password is incorrect');
-		window.location.href='faculty_login.html';
-		</script>";
-      	}
 ?>
-<html>
-	<br><br><br>
-	<input type="button" value="Add an achievement" onclick="window.location.href='a.html'" />
-	<input type="button" value="Logout" onclick="window.location.href='home.html'" />
-</html>
--- Desktop/register.php
+++ Desktop/register.php
@@ -1,11 +1,39 @@
 <?php
-	$connect = new mysqli('localhost','root','','signup');
-	$name=$_POST['name'];
-	$pass=$_POST['pass'];
-	$sql="insert into signup values('$name','$pass')";
-	$query=mysqli_query($connect,$sql);
-	echo "<script>
-		alert('Signup Sucessful!!');
-		window.location.href='faculty_login.html';
-		</script>";
-?>
+session_start();
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$username = $_POST["uname"];
+$pwd = md5($_POST["pwd1"]);
+$email = $_POST["email"];
+$gender = $_POST["gender"];
+$loc = $_POST["location"];
+$conn = new mysqli("localhost:3306","root","1234","saiteja");
+$_SESSION["email"]=$email;
+if($conn->connect_error)
+{
+    die("connection failed".$conn->connect_error);
+}
+else
+{
+    $sqlcheck = "select name from customer where email_address='".$email."';";
+    if($conn->query($sqlcheck)->num_rows)
+    {
+        echo "Account already exists with specified email address";
+        include 'signup.html';
+    }
+    else{
+    $sqli = "insert into customer values('$username','$pwd','$email','$gender','$loc',0);";
+    if($conn->query($sqli)==TRUE)
+    {
+        echo "account successfully created";
+        header("Location:email.php");
+        die();
+    }
+    else {
+        echo "please wait database problems".$conn->error;
+    }
+}
+}
