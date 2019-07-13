f828323c247374dc1a89145a2c925b8d9cc0465a
anilkondapalli99 <anilkondapalli99@gmail.com>
2019-07-13 21:11:02 +0530

    added feedbackform

--- Desktop/login.php
+++ Desktop/login.php
@@ -1,14 +1,15 @@
-<?php
+ <?php
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 session_start();
-$email=$_POST["username"];
+$email=$_POST["email"];
 $pswd=md5($_POST["password"]);
 $_SESSION["email"]=$email;
-$conn = new mysqli("localhost","root","1234","saiteja");
+
+$conn = new mysqli('localhost','root','','cfg');
 if($conn->connect_error)
 {
     die("connection failed ".$conn->connect_error);
@@ -16,7 +17,7 @@ if($conn->connect_error)
 else
 {
     
-    $sql1="select password from customer where email_address='".$email."';";
+    $sql1="select password from userdetails where email='".$email."';";
     $result=$conn->query($sql1);
     
     if($result->num_rows)
